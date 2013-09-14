35b144d632855f408149802fba1df0690c0693b6
Tomás Senart <tomas@soundcloud.com>
2013-09-14 03:32:55 +0100

    Skip certificate validation for HTTPS requests

--- lib/attack_test.go
+++ lib/attack_test.go
@@ -3,6 +3,7 @@ package vegeta
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -20,5 +21,18 @@ func TestAttackRate(t *testing.T) {
 	Attack(Targets{request}, rate, 1*time.Second)
 	if hits := atomic.LoadUint64(&hitCount); hits != rate {
 		t.Fatalf("Wrong number of hits: want %d, got %d\n", rate, hits)
+	}
+}
+
+func TestClientCertConfig(t *testing.T) {
+	server := httptest.NewTLSServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}),
+	)
+	request, _ := http.NewRequest("GET", server.URL, nil)
+	results := make(chan Result, 1)
+	hit(request, results)
+	result := <-results
+	if strings.Contains(result.Error, "x509: certificate signed by unknown authority") {
+		t.Errorf("Invalid certificates should be ignored: Got `%s`", result.Error)
 	}
 }
