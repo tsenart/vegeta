e5f22041c90b8e98f3a3f710bbe4ef1c6b04e921
Tomás Senart <tsenart@gmail.com>
2014-01-21 13:58:50 +0000

    Extend ReportText to display Metrics.P50

--- lib/reporters.go
+++ lib/reporters.go
@@ -20,8 +20,8 @@ func ReportText(results []Result) ([]byte, error) {
 	w := tabwriter.NewWriter(out, 0, 8, 2, '\t', tabwriter.StripEscape)
 	fmt.Fprintf(w, "Requests\t[total]\t%d\n", m.Requests)
 	fmt.Fprintf(w, "Duration\t[total]\t%s\n", m.Duration)
-	fmt.Fprintf(w, "Latencies\t[mean, 95, 99, max]\t%s, %s, %s, %s\n",
-		m.Latencies.Mean, m.Latencies.P95, m.Latencies.P99, m.Latencies.Max)
+	fmt.Fprintf(w, "Latencies\t[mean, 50, 95, 99, max]\t%s, %s, %s, %s, %s\n",
+		m.Latencies.Mean, m.Latencies.P50, m.Latencies.P95, m.Latencies.P99, m.Latencies.Max)
 	fmt.Fprintf(w, "Bytes In\t[total, mean]\t%d, %.2f\n", m.BytesIn.Total, m.BytesIn.Mean)
 	fmt.Fprintf(w, "Bytes Out\t[total, mean]\t%d, %.2f\n", m.BytesOut.Total, m.BytesOut.Mean)
 	fmt.Fprintf(w, "Success\t[ratio]\t%.2f%%\n", m.Success*100)
