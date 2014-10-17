625be5f875d4af07ac732637e049b2fd36719313
Tomás Senart <tsenart@gmail.com>
2014-10-17 19:06:38 +0200

    Simplify file function

--- file.go
+++ file.go
@@ -4,23 +4,16 @@ import (
 	"os"
 )
 
-func file(filename string, create bool) (*os.File, error) {
-	switch filename {
+func file(name string, create bool) (*os.File, error) {
+	switch name {
 	case "stdin":
 		return os.Stdin, nil
 	case "stdout":
 		return os.Stdout, nil
 	default:
-		var file *os.File
-		var err error
 		if create {
-			file, err = os.Create(filename)
-		} else {
-			file, err = os.Open(filename)
+			return os.Create(name)
 		}
-		if err != nil {
-			return nil, err
-		}
-		return file, nil
+		return os.Open(name)
 	}
 }
