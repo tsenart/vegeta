f2a4a593a85c2cc274fc505cf6b56d445338052a
Jacques Fuentes <jpfuentes2@gmail.com>
2014-11-06 12:47:02 -0500

    Add -keepalive flag to toggle persistent conns
    The flag is enabled by default.
    
    closes #89

--- lib/attack.go
+++ lib/attack.go
@@ -99,6 +99,18 @@ func LocalAddr(addr net.IPAddr) func(*Attacker) {
 	}
 }
 
+func KeepAlive(keepalive bool) func(*Attacker) {
+	return func(a *Attacker) {
+		tr := a.client.Transport.(*http.Transport)
+		tr.DisableKeepAlives = !keepalive
+		if !keepalive {
+			a.dialer.KeepAlive = 0
+			tr.Dial = a.dialer.Dial
+		}
+		a.client.Transport = tr
+	}
+}
+
 // TLSConfig returns a functional option which sets the *tls.Config for a
 // Attacker to use with its requests.
 func TLSConfig(c *tls.Config) func(*Attacker) {
