456dd2776cb1b1fd55465f18f9780a466ca33cb0
Jacques Fuentes <jpfuentes2@gmail.com>
2014-11-13 22:03:04 -0500

    Add 300s status code handling for metrics test

--- lib/metrics_test.go
+++ lib/metrics_test.go
@@ -11,13 +11,14 @@ func TestNewMetrics(t *testing.T) {
 	m := NewMetrics(Results{
 		&Result{500, time.Unix(0, 0), 100 * time.Millisecond, 10, 30, "Internal server error"},
 		&Result{200, time.Unix(1, 0), 20 * time.Millisecond, 20, 20, ""},
+		&Result{302, time.Unix(0, 0), 10 * time.Millisecond, 20, 20, ErrTooManyRedirects.Error()},
 		&Result{200, time.Unix(2, 0), 30 * time.Millisecond, 30, 10, ""},
 	})
 
 	for field, values := range map[string][]float64{
 		"BytesIn.Mean":  []float64{m.BytesIn.Mean, 20.0},
 		"BytesOut.Mean": []float64{m.BytesOut.Mean, 20.0},
-		"Sucess":        []float64{m.Success, 0.6666666666666666},
+		"Sucess":        []float64{m.Success, 0.750000},
 	} {
 		if values[0] != values[1] {
 			t.Errorf("%s: want: %f, got: %f", field, values[1], values[0])
@@ -26,7 +27,7 @@ func TestNewMetrics(t *testing.T) {
 
 	for field, values := range map[string][]time.Duration{
 		"Latencies.Max":  []time.Duration{m.Latencies.Max, 100 * time.Millisecond},
-		"Latencies.Mean": []time.Duration{m.Latencies.Mean, 50 * time.Millisecond},
+		"Latencies.Mean": []time.Duration{m.Latencies.Mean, 40 * time.Millisecond},
 		"Latencies.P50":  []time.Duration{m.Latencies.P50, 20 * time.Millisecond},
 		"Latencies.P95":  []time.Duration{m.Latencies.P95, 30 * time.Millisecond},
 		"Latencies.P99":  []time.Duration{m.Latencies.P99, 30 * time.Millisecond},
@@ -39,16 +40,16 @@ func TestNewMetrics(t *testing.T) {
 	}
 
 	for field, values := range map[string][]uint64{
-		"BytesOut.Total": []uint64{m.BytesOut.Total, 60},
-		"BytesIn.Total":  []uint64{m.BytesIn.Total, 60},
-		"Requests":       []uint64{m.Requests, 3},
+		"BytesOut.Total": []uint64{m.BytesOut.Total, 80},
+		"BytesIn.Total":  []uint64{m.BytesIn.Total, 80},
+		"Requests":       []uint64{m.Requests, 4},
 	} {
 		if values[0] != values[1] {
 			t.Errorf("%s: want: %d, got: %d", field, values[1], values[0])
 		}
 	}
 
-	if len(m.StatusCodes) != 2 || m.StatusCodes["200"] != 2 || m.StatusCodes["500"] != 1 {
+	if len(m.StatusCodes) != 3 || m.StatusCodes["200"] != 2 || m.StatusCodes["500"] != 1 || m.StatusCodes["302"] != 1 {
 		t.Errorf("StatusCodes: want: %v, got: %v", map[int]int{200: 2, 500: 1}, m.StatusCodes)
 	}
 
