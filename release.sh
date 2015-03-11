974c4bcc5c86b1785effb2386ced1f617aa68cb4
Tomás Senart <tsenart@gmail.com>
2015-03-11 15:49:21 +0100

    Fix indentation in release.sh

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 for OS in "freebsd" "linux" "darwin" "windows"; do
-	for ARCH in "386" "amd64"; do
-		GOOS=$OS  CGO_ENABLED=0 GOARCH=$ARCH go build -o vegeta
-		ARCHIVE=vegeta-$OS-$ARCH.tar.gz
-		tar -czf $ARCHIVE vegeta
-		echo $ARCHIVE
-	done
+  for ARCH in "386" "amd64"; do
+    GOOS=$OS CGO_ENABLED=0 GOARCH=$ARCH go build -o vegeta
+    ARCHIVE=vegeta-$OS-$ARCH.tar.gz
+    tar -czf $ARCHIVE vegeta
+    echo $ARCHIVE
+  done
 done
