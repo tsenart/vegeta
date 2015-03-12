1341569be2e78548a931ca4f47fc6f1e49a7dede
Tomás Senart <tsenart@gmail.com>
2015-03-12 12:30:13 +0100

    Include version in releases

--- release.sh
+++ release.sh
@@ -2,8 +2,9 @@
 
 for OS in "freebsd" "linux" "darwin" "windows"; do
   for ARCH in "386" "amd64"; do
-    GOOS=$OS CGO_ENABLED=0 GOARCH=$ARCH go build -o vegeta
-    ARCHIVE=vegeta-$OS-$ARCH.tar.gz
+    VERSION="$(git describe --tags $1)"
+    GOOS=$OS CGO_ENABLED=0 GOARCH=$ARCH go build -ldflags "-X main.Version $VERSION" -o vegeta
+    ARCHIVE="vegeta-$VERSION-$OS-$ARCH.tar.gz"
     tar -czf $ARCHIVE vegeta
     echo $ARCHIVE
   done
