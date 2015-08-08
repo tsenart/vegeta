0fb3b2e9951eec802f46aeffc1da3dfc10b9ddea
Tomás Senart <tsenart@gmail.com>
2015-08-08 18:08:59 +0200

    Use R8 method of quantile estimation for latencies
    This change implements R's 8th type of quantile estimation which is
    approximately median-unbiased regardless of the data distribution.

--- lib/metrics_test.go
+++ lib/metrics_test.go
@@ -1,6 +1,7 @@
 package vegeta
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -8,55 +9,40 @@ import (
 func TestNewMetrics(t *testing.T) {
 	t.Parallel()
 
-	m := NewMetrics(Results{
+	want := &Metrics{
+		Latencies: LatencyMetrics{
+			Mean: 112100000,
+			P50:  115000000,
+			P95:  200000000,
+			P99:  200000000,
+			Max:  200000000,
+		},
+		BytesIn:     ByteMetrics{Total: 170, Mean: 17.00},
+		BytesOut:    ByteMetrics{Total: 230, Mean: 23.00},
+		Duration:    9 * time.Second,
+		Wait:        190 * time.Millisecond,
+		Requests:    10,
+		Rate:        1.1111111111111112,
+		Success:     0.9,
+		StatusCodes: map[string]int{"500": 1, "200": 7, "302": 2},
+		Errors:      []string{"Internal server error"},
+	}
+
+	got := NewMetrics(Results{
 		&Result{500, time.Unix(0, 0), 100 * time.Millisecond, 10, 30, "Internal server error"},
 		&Result{200, time.Unix(1, 0), 20 * time.Millisecond, 20, 20, ""},
-		&Result{302, time.Unix(0, 0), 10 * time.Millisecond, 20, 20, ""},
-		&Result{200, time.Unix(2, 0), 30 * time.Millisecond, 30, 10, ""},
+		&Result{302, time.Unix(2, 0), 10 * time.Millisecond, 20, 20, ""},
+		&Result{200, time.Unix(3, 0), 75 * time.Millisecond, 30, 10, ""},
+		&Result{200, time.Unix(4, 0), 200 * time.Millisecond, 20, 20, ""},
+		&Result{200, time.Unix(5, 0), 110 * time.Millisecond, 20, 20, ""},
+		&Result{302, time.Unix(6, 0), 120 * time.Millisecond, 20, 20, ""},
+		&Result{200, time.Unix(7, 0), 130 * time.Millisecond, 30, 10, ""},
+		&Result{200, time.Unix(8, 0), 166 * time.Millisecond, 30, 10, ""},
+		&Result{200, time.Unix(9, 0), 190 * time.Millisecond, 30, 10, ""},
 	})
 
-	for field, values := range map[string][]float64{
-		"Rate":          {m.Rate, 2.0},
-		"BytesIn.Mean":  {m.BytesIn.Mean, 20.0},
-		"BytesOut.Mean": {m.BytesOut.Mean, 20.0},
-		"Sucess":        {m.Success, 0.750000},
-	} {
-		if values[0] != values[1] {
-			t.Errorf("%s: want: %f, got: %f", field, values[1], values[0])
-		}
-	}
-
-	for field, values := range map[string][]time.Duration{
-		"Latencies.Max":  {m.Latencies.Max, 100 * time.Millisecond},
-		"Latencies.Mean": {m.Latencies.Mean, 40 * time.Millisecond},
-		"Latencies.P50":  {m.Latencies.P50, 20 * time.Millisecond},
-		"Latencies.P95":  {m.Latencies.P95, 30 * time.Millisecond},
-		"Latencies.P99":  {m.Latencies.P99, 30 * time.Millisecond},
-		"Duration":       {m.Duration, 2 * time.Second},
-		"Wait":           {m.Wait, 30 * time.Millisecond},
-	} {
-		if values[0] != values[1] {
-			t.Errorf("%s: want: %s, got: %s", field, values[1], values[0])
-		}
-	}
-
-	for field, values := range map[string][]uint64{
-		"BytesOut.Total": {m.BytesOut.Total, 80},
-		"BytesIn.Total":  {m.BytesIn.Total, 80},
-		"Requests":       {m.Requests, 4},
-	} {
-		if values[0] != values[1] {
-			t.Errorf("%s: want: %d, got: %d", field, values[1], values[0])
-		}
-	}
-
-	if len(m.StatusCodes) != 3 || m.StatusCodes["200"] != 2 || m.StatusCodes["500"] != 1 || m.StatusCodes["302"] != 1 {
-		t.Errorf("StatusCodes: want: %v, got: %v", map[int]int{200: 2, 500: 1}, m.StatusCodes)
-	}
-
-	err := "Internal server error"
-	if len(m.Errors) != 1 || m.Errors[0] != err {
-		t.Errorf("Errors: want: %v, got: %v", []string{err}, m.Errors)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 }
 
