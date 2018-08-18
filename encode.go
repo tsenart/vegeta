58922063ce38cc98b17d41fc18b87bed5d93c190
Tomás Senart <tsenart@gmail.com>
2018-08-18 23:58:06 +0200

    encode: Reuse decoder helper in encode

--- encode.go
+++ encode.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -59,42 +58,11 @@ func encodeCmd() command {
 }
 
 func encode(files []string, to, output string) error {
-	srcs := make([]vegeta.Decoder, len(files))
-	decs := []func(io.Reader) vegeta.Decoder{
-		vegeta.NewDecoder,
-		vegeta.NewJSONDecoder,
-		vegeta.NewCSVDecoder,
+	dec, mc, err := decoder(files)
+	defer mc.Close()
+	if err != nil {
+		return err
 	}
-
-	for i, f := range files {
-		in, err := file(f, false)
-		if err != nil {
-			return err
-		}
-		defer in.Close()
-
-		// Auto-detect encoding of each file individually and buffer the read bytes
-		// so that they can be read in subsequent decoding attempts as well as
-		// in the final decoder.
-
-		var buf bytes.Buffer
-		var dec func(io.Reader) vegeta.Decoder
-		for j := range decs {
-			rd := io.MultiReader(bytes.NewReader(buf.Bytes()), io.TeeReader(in, &buf))
-			if err = decs[j](rd).Decode(&vegeta.Result{}); err == nil {
-				dec = decs[j]
-				break
-			}
-		}
-
-		if dec == nil {
-			return fmt.Errorf("encode: can't detect encoding of %q", f)
-		}
-
-		srcs[i] = dec(io.MultiReader(&buf, in))
-	}
-
-	dec := vegeta.NewRoundRobinDecoder(srcs...)
 
 	out, err := file(output, true)
 	if err != nil {
