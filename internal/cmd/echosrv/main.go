0d315c8e151c13efbf15c1e65f5fc5a09973d1ae
Tomás Senart <tsenart@gmail.com>
2018-08-25 11:56:09 +0200

    echosrv: Log request rate to stderr

--- internal/cmd/echosrv/main.go
+++ internal/cmd/echosrv/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"sync/atomic"
+	"time"
 )
 
 func main() {
+	count := uint64(0)
+	go func(last time.Time) {
+		ticks := time.Tick(time.Second)
+		for range ticks {
+			rate := float64(atomic.SwapUint64(&count, 0)) / time.Since(last).Seconds()
+			last = time.Now()
+			log.Printf("Rate: %.3f/s", rate)
+		}
+	}(time.Now())
+
 	http.ListenAndServe(os.Args[1], http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		atomic.AddUint64(&count, 1)
 		bs, _ := httputil.DumpRequest(r, true)
 		w.Write(bs)
 	}))
