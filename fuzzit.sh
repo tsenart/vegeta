32a34d35602f7822e80be355455fc99571082ff8
Tomás Senart <tomas@sourcegraph.com>
2019-10-27 14:15:37 +0100

    fixup! Update fuzzit

--- fuzzit.sh
+++ fuzzit.sh
@@ -17,7 +17,7 @@ export GO111MODULE="off"
 go get -u github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
 go get -d -v -u ./...
 if [ ! -f fuzzit ]; then
-    wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.29/fuzzit_Linux_x86_64
+    wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.75/fuzzit_Linux_x86_64
     chmod a+x fuzzit
 fi
 
