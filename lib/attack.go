tsenart/vegeta

commit 074c53005b0c63139de2450675eed8b248a29cdd

attack: ensure go-routines finish in DNSCaching
Signed-off-by: Tomás Senart <tsenart@gmail.com>

lib/attack.go
@@ -344,23 +344,18 @@ func DNSCaching(ttl time.Duration) func(*Attacker) {
 				for _, ip := range ips {
 					go func(ip string) {
 						conn, err := dial(ctx, network, net.JoinHostPort(ip, port))
+						cancel()
 						ch <- result{conn, err}
 					}(ip)
 				}
 
 				for i := 0; i < cap(ch); i++ {
-					select {
-					case <-ctx.Done():
-						return nil, ctx.Err()
-					case r := <-ch:
-						if err = r.err; err != nil {
-							continue
-						}
-						return r.conn, nil
+					if r := <-ch; conn == nil {
+						conn, err = r.conn, r.err
 					}
 				}
 
-				return nil, err
+				return conn, err
 			}
 		}
 	}

commit e49c367a014978b5b7228d0e2d6bed6c0555e1e3

attack: early cancel ctx only when there's no err
This fixes a bug introduced in 074c530 where we'd be cancelling the
racing go-routines even when the first one produced an error, hence
returning an error.

Signed-off-by: Tomás Senart <tsenart@gmail.com>

lib/attack.go
@@ -356,7 +356,9 @@ func DNSCaching(ttl time.Duration) func(*Attacker) {
 				for _, ip := range ips {
 					go func(ip string) {
 						conn, err := dial(ctx, network, net.JoinHostPort(ip, port))
-						cancel()
+						if err == nil {
+							cancel()
+						}
 						ch <- result{conn, err}
 					}(ip)
 				}